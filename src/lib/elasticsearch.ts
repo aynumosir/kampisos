605dbeb237d954ff5dd60d5d0bc7777aa2b9066b
Ryo Igarashi <n33t5hin@gmail.com>
2026-05-07 10:09:40 +0900

    support api key auth

--- src/lib/elasticsearch.ts
+++ src/lib/elasticsearch.ts
@@ -2,13 +2,20 @@ import { Client } from "@elastic/elasticsearch";
 import assert from "assert";
 
 assert(process.env.ELASTICSEARCH_ENDPOINTS);
-assert(process.env.ELASTICSEARCH_USERNAME);
-assert(process.env.ELASTICSEARCH_PASSWORD);
+
+let auth: ConstructorParameters<typeof Client>[0]["auth"];
+if (process.env.ELASTICSEARCH_PASSWORD) {
+  auth = {
+    username: process.env.ELASTICSEARCH_USERNAME!,
+    password: process.env.ELASTICSEARCH_PASSWORD!,
+  };
+} else {
+  auth = {
+    apiKey: process.env.ELASTICSEARCH_API_KEY!,
+  };
+}
 
 export const client = new Client({
   nodes: process.env.ELASTICSEARCH_ENDPOINTS.split(","),
-  auth: {
-    username: process.env.ELASTICSEARCH_USERNAME,
-    password: process.env.ELASTICSEARCH_PASSWORD,
-  },
+  auth,
 });
